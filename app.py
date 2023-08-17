d8122563c6a8490fee2aa3892752d60be5d4546f
Instrutor <sophia.sales@aluno.senai.br>
2023-08-17 08:28:14 -0300

    aula1

--- app.py
+++ app.py
@@ -5,14 +5,14 @@ def create_app(): # cria uma função para definir o aplicativo
     
     @app.route("/") # cria uma rota
     def index(): # função que gerencia rota
-        return render_template("index.html") # combina o python com html
+        nome = "Sophia657"
+        return render_template("index.html", nome=nome) # combina o python com html
 
     @app.route("/alunos")
     def alunos():
         import json
         from database.dados import alunos
-        return alunos
-
+        return render_template("lista.html", alunos=alunos )
     @app.route("/login")
     def login():
         return "<H1>Login ainda não implementado</h1>"
