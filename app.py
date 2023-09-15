109d2679cdebd1e2f8091d5e552deb93c62c4d6a
Instrutor <anna.santos36@aluno.senai.br>
2023-09-15 10:25:23 -0300

    local changes

--- app.py
+++ app.py
@@ -1,19 +1,36 @@
-from flask import Flask, render_template # importa bibliotecas
+from flask import Flask, redirect, render_template, request, session, url_for # importa bibliotecas
 
 def create_app(): # cria uma função para definir o aplicativo
     app = Flask(__name__) # instancia o Flask
     
     @app.route("/") # cria uma rota
     def index(): # função que gerencia rota
-        return render_template("index.html") # combina o python com html
+        nome = "Anna 005"
+        return render_template("index.html", nome=nome) # combina o python com html
 
     @app.route("/alunos")
     def alunos():
         import json
         from database.dados import alunos
-        return alunos
+        return render_template("lista.html" , alunos=alunos)
 
-    @app.route("/login")
+    @app.route("/login", methods=('post', 'GET'))
+    def login():
+        error = None
+        if request.method == 'POST':
+            email = request.form.get('email')
+            senha = request.form.get('senha')
+            
+            from database.dados import alunos
+            for k,v in alunos.items():
+                if email == v.get('usuario') and senha == v.get('senha'): 
+                    session['user'] = v
+                    return redirect(url_for('index'))
+                else:
+                    error = "Usuario ou senha invalido!"
+           
+        return render_template("login.html" , error=error)
+
     def login():
         return "<H1>Login ainda não implementado</h1>"
     
