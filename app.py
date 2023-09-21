23c3a88b7007ca00ea70f6d0bc205caa81296e49
Instrutor <beatriz.silva14@portalsesisp.org.br>
2023-09-21 08:19:45 -0300

    locals

--- app.py
+++ app.py
@@ -1,21 +1,35 @@
-from flask import Flask, render_template # importa bibliotecas
+from flask import Flask, redirect, render_template, request, session, url_for # importa bibliotecas
 
 def create_app(): # cria uma função para definir o aplicativo
     app = Flask(__name__) # instancia o Flask
     
     @app.route("/") # cria uma rota
     def index(): # função que gerencia rota
-        return render_template("index.html") # combina o python com html
+        nome = "Beatriz"
+        return render_template("index.html", nome=nome) # combina o python com html
 
     @app.route("/alunos")
     def alunos():
         import json
         from database.dados import alunos
-        return alunos
+        return render_template("lista.html" , alunos=alunos)
 
-    @app.route("/login")
+    @app.route("/login" , methods=('POST', 'GET'))
     def login():
-        return "<H1>Login ainda não implementado</h1>"
+        if request.method == 'POST':
+            email = request.form.get('email')
+            senha = request.form.get('senha')
+
+            from database.dados import alunos
+            for k,v in alunos.items():
+                if email == v.get('usuario') and senha == v.get('senha'):
+                    session['user'] = v
+                    return redirect(url_for('index'))
+                else:
+                    error = "Usuario ou senha invalidos!
+            
+            
+        return render_template("login.html")
     
     return app # retorna o app criado
 
