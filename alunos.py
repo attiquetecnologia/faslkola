9fc2f17e31ad24951e5d6d53fb618e1af8b3daec
Instrutor <pedrocsilva@aluno.senai.br>
2023-09-28 12:00:49 -0300

    lista de alunos pre-definidos

--- alunos.py
+++ alunos.py
@@ -4,7 +4,14 @@ bp = Blueprint("Aluno", __name__)
 
 @bp.route("/alunos/lista")
 def lista():
-    return render_template("alunos/lista.html")
+    from database.dados import alunos
+
+    # Função lambda cria funções de 1 linha só
+    # Media = lambda t,p1,p2: t*.3+p1*.35+p2*.35
+    def media(t, p1, p2):
+        return t*.3+p1*.35+p2*.35
+     
+    return render_template("alunos/lista.html",alunos = alunos)
 
 @bp.route("/alunos/add")
 def add():
