6b89fe3d5c91e903671aaccf0fb49da705f48ba4
Leandro Giovani <127615704+LeandroGiovani@users.noreply.github.com>
2023-09-28 08:35:18 -0300

    lista de alunos e botões

---
+++ alunos.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, render_template
+
+bp = Blueprint("Aluno", __name__)
+
+@bp.route("/alunos/lista")
+def lista():
+    from database.dados import alunos
+
+    def media(t, p1 , p2):
+        return t*.3+p1*.35+p2*.35
+
+    return render_template("alunos/lista.html", media=media, alunos=alunos)
+
+@bp.route("/alunos/add")
+def add():
+    return render_template("alunos/form.html")
+
+@bp.route("/alunos/<int:id>delete")
+def delete(id):
+    return render_template("alunos/delete.html")
+
+@bp.route("/alunos/<int:id>edit")
+def edit(id):
+    return render_template("alunos/form.html")
