774ab35ef0380608ef272cd241b34ab924f14d78
Instrutor <luiz.arduino@aluno.senai.br>
2023-09-29 15:11:40 -0300

    CRUD finalizado

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template # importa bibliotecas
+from flask import Flask, redirect, render_template, request, session, url_for # importa bibliotecas
 
 def create_app(): # cria uma função para definir o aplicativo
     app = Flask(__name__) # instancia o Flask
     
     @app.route("/") # cria uma rota
     def index(): # função que gerencia rota
-        nome = "Rodrigo 123"
+        nome = "Barbosa 111"
         return render_template("index.html", nome=nome) # combina o python com html
 
     @app.route("/alunos")
@@ -20,11 +20,57 @@ def create_app(): # cria uma função para definir o aplicativo
         
         return render_template("lista.html", alunos=alunos, media=media )
 
-    @app.route("/login")
+    @app.route("/login", methods=('POST', 'GET'))
     def login():
-        return "<H1>Login ainda não implementado</h1>"
+        error = None
+        if request.method == 'POST':
+            email = request.form.get('email')
+            senha = request.form.get('senha')
+
+            from database.dados import alunos
+            for k,v in alunos.items():
+                if email == v.get('usuario') and senha == v.get('senha'):
+                    session['user'] = v
+                    return redirect(url_for('index'))
+                else:
+                    error = "Usuario ou senha inválidos!"
+
+        return render_template("login.html", error=error)
+    @app.route("/registro", methods=('POSTE', 'GET'))
+    def registro():
+        return render_template("registro.html")
+    
+    @app.route("/logout", methods=('POSTE', 'GET'))
+    def logout():
+        return redirect(url_for("login"))
+    
+    @app.route("/lost_pass", methods=('POSTE', 'GET'))
+    def lost_pass():
+        return render_template("lost_pass.html")
+    
+    @app.route("/perfil", methods=('POSTE', 'GET'))
+    def perfil():
+        
+        # 1-Pegar dados do banco
+
+        # for k,v in alunos... session ['user']['email']==email
+        
+        if request.method == "POST":
+            #logica salvar
+            nome = request.form.get("nome")
+        return render_template("perfil.html")
+    from alunos import bp
+    app.register_blueprint(bp)
     
     return app # retorna o app criado
+    
+    @app.route("/recuperarsenha", methods=('POST', 'GET'))
+    def recuperarsenha():
+        error = None
+        return render_template("recuperarsenha.html", error=error)
+
+    
+
 
 if __name__ == "__main__": # 'função principal' do python
     create_app().run(debug=True) # executa o flask na porta http://127.0.0.1:5000
