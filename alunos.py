89b3ca6ab54ee0c4840249bd4562887fd3abe5dd
Instrutor <pedrocsilva@aluno.senai.br>
2023-09-29 09:56:32 -0300

    Editar aluno ok

--- alunos.py
+++ alunos.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect
 
 bp = Blueprint("Aluno", __name__)
 
@@ -11,16 +11,55 @@ def lista():
     def media(t, p1, p2):
         return t*.3+p1*.35+p2*.35
      
-    return render_template("alunos/lista.html",alunos = alunos)
+    return render_template("alunos/lista.html",alunos = alunos , media=media)
 
 @bp.route("/alunos/add")
 def add():
     return render_template("alunos/form.html")
 
-@bp.route("/alunos/<int:id>delete")
+@bp.route("/alunos/<int:id>delete", methods=("GET", "POST"))
 def delete(id):
-    return render_template("alunos/delete.html")
+    from database.dados import alunos
 
-@bp.route("/alunos/<int:id>/edit")
+    aluno = alunos.get(id)
+
+    if request.method == "POST" and request.form.get("apagar") == "sim":
+        del alunos[id] # deleta o aluno do dicionário
+        return redirect(url_for("Aluno.lista"))
+
+    return render_template("alunos/delete.html", id=id, aluno=aluno)
+
+@bp.route("/alunos/<int:id>/edit", methods=("GET", "POST"))
 def edit(id):
-    return render_template("alunos/form.html")
+    from database.dados import alunos
+    erros= []
+    aluno = alunos.get(id)
+
+    if request.method=="POST":
+        nome = request.form.get("nome")
+        usuario = request.form.get("email")
+        t = float(request.form.get("t"))
+        p1 = float(request.form.get("p1"))
+        p2 = float(request.form.get("p2"))
+
+        erros = []
+        # Validações
+        if not nome: erros.append("Nome é um campo obrigatório")
+        if not usuario: erros.append("Email é um campo obrigatório")
+        if not t or t <0 or t > 10: erros.append("trabalho é um campo obrigatório ou valores não entre 0-10")
+        if not p1 or p1 <0 or p1 > 10: erros.append("Prova 1 é um campo obrigatório ou valores não entre 0-10")
+        if not p2 or p2 <0 or p2 > 10: erros.append("Prova 2 é um campo obrigatório ou valores não entre 0-10")
+
+    if len(erros) == 0:
+        # altera usuário no banco de dados
+        alunos[id]["nome"] = nome
+        alunos[id]["usuario"] = usuario
+        alunos[id]["t"] = t
+        alunos[id]["p1"] = p1
+        alunos[id]["p2"] = p2
+
+        flash(f"Usuário {nome}, salvo com sucesso!")
+        
+        return redirect(url_for("Aluno.edit", id=id))
+
+    return render_template("alunos/form.html", id=id, aluno=aluno, erros=erros)
