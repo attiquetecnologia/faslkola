c40acee3e56b6c0569021fae981bc6dc3822abb7
Instrutor <Larissa.dassie@aluno.senai.br>
2023-09-29 08:38:01 -0300

    start blueprint alunos

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template, request, session, url_for 
+from flask import Flask, app, redirect, render_template, request, session, url_for 
 
 def create_app(): # cria uma função para definir o aplicativo
     app = Flask(__name__) # instancia o Flask
@@ -36,8 +36,11 @@ def create_app(): # cria uma função para definir o aplicativo
                     error = "Usuario ou senha invalidos!"
 
         return render_template("login.html" , error=error)
-    
+    from alunos import bp
+    app.register_blueprint(bp)
+
     return app # retorna o app criado
 
 if __name__ == "__main__": # 'função principal' do python
     create_app().run(debug=True) # executa o flask na porta http://127.0.0.1:5000
+    
