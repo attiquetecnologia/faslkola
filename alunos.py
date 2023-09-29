59e17bb74bb0bb7c9d800ae282affa237f32e8ee
Leandro Giovani <127615704+LeandroGiovani@users.noreply.github.com>
2023-09-29 10:37:24 -0300

    add ok

--- alunos.py
+++ alunos.py
@@ -11,9 +11,37 @@ def lista():
 
     return render_template("alunos/lista.html", media=media, alunos=alunos)
 
-@bp.route("/alunos/add")
+@bp.route("/alunos/add", methods=("POST", "GET"))
 def add():
-    return render_template("alunos/form.html")
+    from database.dados import alunos
+    erros = []
+
+    if request.method=="POST":
+        nome = request.form.get("nome")
+        usuario = request.form.get("email")
+        t = float(request.form.get("t"))
+        p1 = float(request.form.get("p1"))
+        p2 = float(request.form.get("p2"))
+        
+        if not nome: erros.append("Nome é um campo obrigatório")
+        if not usuario: erros.append("Email é um campo obrigatório")
+        if not t or t<0 or t>10: erros.append("Trabalho é um campo obrigatório ou valores não entre 0-10")
+        if not p1 or p1<0 or p1>10: erros.append("Prova 1 é um campo obrigatório ou valores não entre 0-10")
+        if not p2 or p2<0 or p2>10: erros.append("Prova 2 é um campo obrigatório ou valores não entre 0-10")
+    
+        if usuario in str(alunos): erros.append("Email já registrado")
+        
+        if len(erros) == 0:
+            last_id = max(alunos.keys())
+            id = last_id+1
+
+            alunos[id] = {"nome": nome, "usuario": usuario, "t": t, "p1": p1, "p2": p2}
+
+            flash(f"Usuário {nome}, salvo com sucesso!")
+            
+        return redirect(url_for("Aluno.edit", id=id))
+    
+    return render_template("alunos/form.html", erros=erros)
 
 @bp.route("/alunos/<int:id>/delete", methods=("GET", "POST"))
 def delete(id):
@@ -46,8 +74,10 @@ def edit(id):
         if not t or t<0 or t>10: erros.append("Trabalho é um campo obrigatório ou valores não entre 0-10")
         if not p1 or p1<0 or p1>10: erros.append("Prova 1 é um campo obrigatório ou valores não entre 0-10")
         if not p2 or p2<0 or p2>10: erros.append("Prova 2 é um campo obrigatório ou valores não entre 0-10")
+
     
         if len(erros) == 0:
+
             alunos[id]["nome"] = nome
             alunos[id]["usuario"] = usuario
             alunos[id]["t"] = t
@@ -56,6 +86,6 @@ def edit(id):
 
             flash(f"Usuário {nome}, salvo com sucesso!")
             
-        return redirect(url_for("Aluno.lista", id=id))
+        return redirect(url_for("Aluno.edit", id=id))
     
-    return render_template("alunos/form.html", id=id, aluno=aluno, erros=erros)
+    return render_template("alunos/form.html", aluno=aluno, erros=erros)
