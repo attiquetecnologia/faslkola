0a0400a39f05736bed65a809794a0d5ad15caf9d
Instrutor <marilia.rodrigues@aluno.sesi.br>
2023-10-03 15:02:27 -0300

    lista de alunos pré-definidos

--- app.py
+++ app.py
@@ -1,24 +1,18 @@
-from flask import Flask, redirect, render_template, request, session, url_for
+from flask import Flask, redirect, render_template, request, session, url_for # importa bibliotecas
 
 def create_app(): # cria uma função para definir o aplicativo
-    app = Flask(__name__) # instancia o Flask
+    app = Flask(_name_) # instancia o Flask
     app.secret_key = "abax"
     @app.route("/") # cria uma rota
     def index(): # função que gerencia rota
-        nome = "Rodrigo 123"
+        nome = "Ana Clara"
         return render_template("index.html", nome=nome) # combina o python com html
 
     @app.route("/alunos")
     def alunos():
         import json
         from database.dados import alunos
-
-        # Função lambda cria funções de 1 linha só
-        # media = lambda t,p1,p2: t*.3+p1*.35+p2*.35
-        def media(t, p1, p2):
-            return t*.3+p1*.35+p2*.35
-        
-        return render_template("lista.html", alunos=alunos, media=media )
+        return render_template("lista.html", alunos=alunos )
 
     @app.route("/login", methods=('POST', 'GET'))
     def login():
@@ -32,12 +26,14 @@ def create_app(): # cria uma função para definir o aplicativo
                 if email == v.get('usuario') and senha == v.get('senha'):
                     session['user'] = v
                     return redirect(url_for('index'))
-                else:
-                    error = "Usuario ou senha inválidos!"
+            else:
+                error = "usuario ou senha inválidos!"
 
-        return render_template("login.html", error=error)
-    
+        return render_template("login.html", error=error) 
+    from alunos import bp 
+    app.register_blueprint(bp)
+
     return app # retorna o app criado
 
-if __name__ == "__main__": # 'função principal' do python
+if _name_ == "_main_": # 'função principal' do python
     create_app().run(debug=True) # executa o flask na porta http://127.0.0.1:5000
