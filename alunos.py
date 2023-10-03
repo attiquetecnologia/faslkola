8edc01af366ac64829f905bcd8e7e10ea5b39a29
Ana Clara <ana.faustinoni@portalsesi.org.br>
2023-10-03 13:26:00 -0300

    CRUD finalizado bug corrigido

--- alunos.py
+++ alunos.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, request, url_for
+from flask import Blueprint, render_template, redirect, request, url_for, flash
 
 bp = Blueprint("Aluno", __name__)
 
