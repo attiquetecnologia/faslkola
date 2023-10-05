857bd98dec71e6e96583c9e63dfee1a048d3aff1
Instrutor <luis.robert@aluno.senai.br>
2023-10-05 08:47:27 -0300

    Requirements e SQLAlchemy

--- app.py
+++ app.py
@@ -8,63 +8,10 @@ def create_app(): # cria uma função para definir o aplicativo
         nome = "Luis Fernando"
         return render_template("index.html", nome=nome) # combina o python com html
 
-    @app.route("/alunos")
-    def alunos():
-        import json
-        from database.dados import alunos
+    from usuarios.controller import bp
+    app.register_blueprint(bp)
 
-        # Função lambda cria funções de 1 linha só
-        # media = lambda t,p1,p2: t*.3+p1*.35+p2*.35
-        def media(t, p1, p2):
-            return t*.3+p1*.35+p2*.35
-        
-        return render_template("lista.html", alunos=alunos, media=media )
-
-    @app.route("/login", methods=('POST', 'GET'))
-    def login():
-        error = None
-        if request.method == 'POST':
-            email = request.form.get('email')
-            senha = request.form.get('senha')
-
-            from database.dados import alunos
-            for k,v in alunos.items():
-                if email == v.get('usuario') and senha == v.get('senha'):
-                    session['user'] = v
-                    return redirect(url_for('index'))
-                else:
-                    error = "Usuario ou senha inválidos!"
-
-        return render_template("login.html", error=error)
-    @app.route("/registro", methods=('POSTE', 'GET'))
-    def registro():
-        return render_template("registro.html")
-    
-    @app.route("/logout", methods=('POSTE', 'GET'))
-    def logout():
-        return redirect(url_for("login"))
-    
-    @app.route("/lost_pass", methods=('POSTE', 'GET'))
-    def lost_pass():
-        return render_template("lost_pass.html")
-    
-    @app.route("/perfil", methods=('POSTE', 'GET'))
-    def perfil():
-        
-        # 1-Pegar dados do banco
-
-        # for k,v in alunos... session ['user']['email']==email
-        
-        if request.method == "POST":
-            #logica salvar
-            nome = request.form.get("nome")
-        return render_template("perfil.html")
     return app # retorna o app criado
     
-    @app.route("/recuperarsenha", methods=('POST', 'GET'))
-    def recuperarsenha():
-        error = None
-        return render_template("recuperarsenha.html", error=error)
-
 if __name__ == "__main__": # 'função principal' do python
     create_app().run(debug=True) # executa o flask na porta http://127.0.0.1:5000
---
+++ usuarios/controller.py
@@ -0,0 +1,38 @@
+from flask import Blueprint, request, url_for, session, redirect, render_template
+
+bp = Blueprint("Usuario", __name__)
+
+@bp.route("/login", methods=('POST', 'GET'))
+def login():
+    error = None
+    if request.method == 'POST':
+        email = request.form.get('email')
+        senha = request.form.get('senha')
+
+        from database.dados import alunos
+        for k,v in alunos.items():
+            if email == v.get('usuario') and senha == v.get('senha'):
+                session['user'] = v
+                return redirect(url_for('index'))
+            else:
+                error = "Usuario ou senha inválidos!"
+
+    return render_template("usuarios/login.html", error=error)
+
+@bp.route("/logout")
+def logout():
+    return redirect(url_for("login"))
+
+@bp.route("/perfil", methods=('POSTE', 'GET'))
+def perfil():
+        
+        # 1-Pegar dados do banco
+
+        # for k,v in alunos... session ['user']['email']==email
+        
+        if request.method == "POST":
+            #logica salvar
+            nome = request.form.get("nome")
+        return render_template("perfil.html")
+
+    
