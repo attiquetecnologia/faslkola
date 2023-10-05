edd04c7880b471ae241aeabec4a30a3a19bd682b
Ana Clara <ana.faustinoni@portalsesi.org.br>
2023-10-05 09:14:47 -0300

    Deploy MVC para expansão do app

--- app.py
+++ app.py
@@ -8,29 +8,11 @@ def create_app(): # cria uma função para definir o aplicativo
         nome = "Ana Clara"
         return render_template("index.html", nome=nome) # combina o python com html
 
-    @app.route("/alunos")
-    def alunos():
-        import json
-        from database.dados import alunos
-        return render_template("lista.html", alunos=alunos )
 
-    @app.route("/login", methods=('POST', 'GET'))
-    def login():
-        error = None
-        if request.method == 'POST':
-            email = request.form.get('email')
-            senha = request.form.get('senha')
-
-            from database.dados import alunos
-            for k,v in alunos.items():
-                if email == v.get('usuario') and senha == v.get('senha'):
-                    session['user'] = v
-                    return redirect(url_for('index'))
-            else:
-                error = "usuario ou senha inválidos!"
-
-        return render_template("login.html", error=error) 
-    from alunos import bp 
+    from usuarios.controller import bp
+    app.register_blueprint(bp)
+    
+    from alunos.controller import bp 
     app.register_blueprint(bp)
 
     return app # retorna o app criado
