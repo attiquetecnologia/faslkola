4e5235df804a5be9002c2e8371e967726f3e465c
Instrutor <marilia.rodrigues@aluno.sesi.br>
2023-10-05 09:39:31 -0300

    requirements e SQLAlchemy

--- app.py
+++ app.py
@@ -1,39 +1,33 @@
-from flask import Flask, redirect, render_template, request, session, url_for # importa bibliotecas
+from flask import Flask, redirect, render_template, request, session, url_for
 
 def create_app(): # cria uma função para definir o aplicativo
-    app = Flask(_name_) # instancia o Flask
+    app = Flask(__name__) # instancia o Flask
     app.secret_key = "abax"
     @app.route("/") # cria uma rota
     def index(): # função que gerencia rota
-        nome = "Ana Clara"
+        nome = "Marilia"
         return render_template("index.html", nome=nome) # combina o python com html
 
     @app.route("/alunos")
     def alunos():
         import json
         from database.dados import alunos
-        return render_template("lista.html", alunos=alunos )
 
-    @app.route("/login", methods=('POST', 'GET'))
-    def login():
-        error = None
-        if request.method == 'POST':
-            email = request.form.get('email')
-            senha = request.form.get('senha')
+        # Função lambda cria funções de 1 linha só
+        # media = lambda t,p1,p2: t*.3+p1*.35+p2*.35
+        def media(t, p1, p2):
+            return t*.3+p1*.35+p2*.35
+        
+        return render_template("lista.html", alunos=alunos, media=media )
 
-            from database.dados import alunos
-            for k,v in alunos.items():
-                if email == v.get('usuario') and senha == v.get('senha'):
-                    session['user'] = v
-                    return redirect(url_for('index'))
-            else:
-                error = "usuario ou senha inválidos!"
-
-        return render_template("login.html", error=error) 
+    
     from alunos import bp 
     app.register_blueprint(bp)
 
+    from usuarios.controller import bp
+    app.register_blueprint(bp)
+    
     return app # retorna o app criado
 
-if _name_ == "_main_": # 'função principal' do python
+if __name__ == "__main__": # 'função principal' do python
     create_app().run(debug=True) # executa o flask na porta http://127.0.0.1:5000
---
+++ usuarios/controller.py
@@ -0,0 +1,20 @@
+from flask import Blueprint, request, url_for, redirect, flash, render_template, session
+
+bp = Blueprint("Usuario", __name__)
+
+@bp.route("/login", methods=('POST', 'GET'))
+def login():
+    error = None
+    if request.method == 'POST':
+        email = request.form.get('email')
+        senha = request.form.get('senha')
+
+        from database.dados import alunos
+        for k,v in alunos.items():
+            if email == v.get('usuario') and senha == v.get('senha'):
+                session['user'] = v
+                return redirect(url_for('index'))
+            else:
+                error = "Usuario ou senha inválidos!"
+
+    return render_template("usuarios/login.html", error=error)
