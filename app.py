1eedb337ad474e6aef2957368e35df2be5a3e42f
Instrutor <gustavo.farias3@aluno.senai.br>
2023-10-06 13:30:40 -0300

    banco de dados ok

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask, redirect, render_template, request, session, url_for
+import click
+from flask import Flask, render_template
+from flask.cli import with_appcontext 
+from database.connection import db 
 
 def create_app(): # cria uma função para definir o aplicativo
     app = Flask(__name__) # instancia o Flask
     app.secret_key = "abax"
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqldb://root:5e5i_123@localhost:3306/flaskola"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    app.cli.add_command(init_db_command)
+
     @app.route("/") # cria uma rota
     def index(): # função que gerencia rota
-        nome = "Marilia"
+        nome = "Gustavo"
         return render_template("index.html", nome=nome) # combina o python com html
 
     @app.route("/alunos")
@@ -21,13 +31,28 @@ def create_app(): # cria uma função para definir o aplicativo
         return render_template("lista.html", alunos=alunos, media=media )
 
     
-    from alunos import bp 
+    from alunos.controller import bp 
     app.register_blueprint(bp)
 
     from usuarios.controller import bp
     app.register_blueprint(bp)
     
     return app # retorna o app criado
+
+def init_db():
+    db.drop_all()
+    # db.creat_all()
+    db.reflect()
+    
+
+@click.command("init-db")
+@with_appcontext
+def init_db_command():
+    """Clear existing data and create new tables."""
+
+    init_db()
+    click.echo("initialized the database.")
+
 
 if __name__ == "__main__": # 'função principal' do python
     create_app().run(debug=True) # executa o flask na porta http://127.0.0.1:5000
