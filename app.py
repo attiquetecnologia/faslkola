c09273c20b1017522d4c8c858567951f4b3ee78e
Instrutor <you@example.com>
2023-10-06 08:09:31 -0300

    conexão com banco ok

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
+import click
 from flask import Flask, redirect, render_template, request, session, url_for, flash 
+from flask import Flask, render_template
+from database.connection import db
 
 def create_app(): # cria uma função para definir o aplicativo
     app = Flask(__name__) # instancia o Flask
     app.secret_key = "abax"
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqldb://usuario:senha@localhost:3306/banco_dados"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    app.cli.add_command(init_db_command)
+
     @app.route("/") # cria uma rota
     def index(): # função que gerencia rota
         nome = "Rodrigo 123"
@@ -14,6 +24,21 @@ def create_app(): # cria uma função para definir o aplicativo
     from alunos.controller import bp
 
     return app # retorna o app criado
+
+def init_db():
+    db.drop_all()
+    # db.create_all()
+    db.reflect()
+
+
+    @click.command("init-db")
+    @with_appcontext
+    def init_db_command():
+        """Clear existing data and create new tables."""
+
+        init_db()
+        click.echo("Initialized the database.")
+        
     
 if __name__ == "__main__": # 'função principal' do python
     create_app().run(debug=True) # executa o flask na porta http://127.0.0.1:5000
