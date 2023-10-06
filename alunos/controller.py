b76eb227203ba1ed2643e7cdc4098842e8c50477
Instrutor <you@example.com>
2023-10-06 14:44:39 -0300

    lista alunos funcionando

--- alunos/controller.py
+++ alunos/controller.py
@@ -1,21 +1,20 @@
 from flask import Blueprint, flash, redirect, render_template, request, url_for
-from sqlalchemy import text
+from sqlalchemy import select
 from database.connection import db
+from .model import Aluno
 
 bp = Blueprint("Aluno", __name__)
 
 @bp.route("/alunos/lista")
 def lista(): 
-    from database.dados import alunos
-    lista = db.session.execute(text("Select * from alunos"))
+    lista = db.session.scalars(select(Aluno))
 
     # função lamba cria funções de 1 linha só
     # média = lamba t, p1, p2: t*.3+p1* .35+p2*.35
     def media(t, p1, p2):
         return t*.3+p1*.35+p2*.35
     
-
-    return render_template("alunos/lista.html", lista=lista, alunos=alunos, media=media)
+    return render_template("alunos/lista.html", lista=lista, media=media)
 
 @bp.route("/alunos/add", methods=("GET", "POST")) 
 def add():
