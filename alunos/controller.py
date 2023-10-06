15c0cff50679f04b577ae49786a7b2ec690b8496
Instrutor <you@example.com>
2023-10-06 15:33:56 -0300

    CRUD com banco de dados funcionando!

--- alunos/controller.py
+++ alunos/controller.py
@@ -51,13 +51,14 @@ def add():
 
 @bp.route("/alunos/<int:id>/delete", methods=("GET", "POST"))
 def delete(id): 
-    from database.dados import alunos
-
-    aluno = alunos.get(id)
+    aluno = Aluno.query.filter_by(id=id).first()
 
     if request.method == "POST" and request.form.get("apagar") == "sim":
-        del alunos[id]
-        return redirect(url_for("Aluno.lista"))
+        
+       db.session.delete(aluno) # deleta o aluno
+       db.session.commit()
+
+       return redirect(url_for("Aluno.lista"))
 
     return render_template("alunos/delete.html", id=id, aluno=aluno)
 
