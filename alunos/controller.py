6a100ad8c1cd9abb6981d597f7e80bb4c733cf57
Instrutor <luiz.arduino@aluno.senai.br>
2023-10-06 07:47:05 -0300

    Adicionar novos alunos funcionando

--- alunos/controller.py
+++ alunos/controller.py
@@ -17,35 +17,34 @@ def lista():
 
 @bp.route("/alunos/add", methods=("GET", "POST"))   
 def add ():
-    from database.dados import alunos
     erros = []
     
 
     if request.method=="POST":
         nome = request.form.get("nome")
-        usuario = request.form.get("email")
+        email = request.form.get("email")
         t = float(request.form.get("t"))
         p1 = float(request.form.get("p1"))
         p2 = float(request.form.get("p2"))
 
         
         if not nome: erros.append("Nome é um campo obrigatório")
-        if not usuario: erros.append("Email é um campo obrigatório")
+        if not email: erros.append("Email é um campo obrigatório")
         if not t or t <0 or t> 10: erros.append("Trabalho é um campo obrigatório ou valores não entre 0-10")
         if not p1 or p1 <0 or p1> 10: erros.append("Prova 1 é um campo obrigatório ou valores não entre 0-10")
         if not p2 or p2 <0 or p2> 10: erros.append("Prova 2 é um campo obrigatório ou valores não entre 0-10")
 
-        #verifica se ja existe
-        if usuario in str(alunos): erros.append("Email ja registradot56")
+
         if len(erros) == 0:
-            last_id = max(alunos.keys())
-            id = last_id+1
             # salva usuário no banco de dados
-            alunos[id] = {"nome": nome, "usuario": usuario, "t": t, "p1": p1, "p2": p2 }
+            aluno = Aluno(**{"nome": nome, "email": email, "t": t, "p1": p1, "p2": p2 })
+            db.session.add(aluno)
+            db.session.commit() #persiste no banco
 
             flash(f"Usuário {nome}, salvo com sucesso!")
 
-            return redirect(url_for("Aluno.edit, id=id"))
+            return redirect(url_for("Aluno.edit, id=aluno.id"))
+        
     return render_template("alunos/form.html", erros=erros)
 
 
